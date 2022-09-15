82cb86d804d54602b2b08d7c0022ac3e219b1330
csg0629 <3305970059>
2022-09-15 15:06:02 +0800

    提交项目框架文件

---
+++ src/com/hqzy/servlet/Servlet1.java
@@ -0,0 +1,23 @@
+package com.hqzy.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/Servlet111/*","*.do"},
+        initParams = {@WebInitParam(name = "encoding",value = "utf-8")})
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("servlet 222222222221@@@1111");
+        System.out.println(this.getInitParameter("encoding"));
+        System.out.println(request.getParameter("encoding"));
+    }
+}
